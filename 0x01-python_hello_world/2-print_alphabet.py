c86b8b56e757190596f4762a9d50a3dcc60aeb3f
ibex214 <kdbraswell@gmail.com>
2018-06-03 23:51:17 +0000

    upload exercises

---
+++ 0x01-python_hello_world/2-print_alphabet.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+
+
+# alpha = 'abcdefghijklmnopqrstuvwxyz'
+for i in range(26):
+    print(chr(97+i),end="")
+    i = i + 1
---
+++ 0x01-python_hello_world/3-print_alphabet.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+# alpha = 'abcdefghijklmnopqrstuvwxyz'
+for i in range(26):
+    if chr(97+i) == "q":
+        pass
+    elif chr(97+i) == "e":
+        pass
+    else:
+        print(chr(97+i),end="")
+    i = i + 1
